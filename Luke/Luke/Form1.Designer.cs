60627d4f5d692a945cabd1b7934b7f61ec498423
Kokhanchik Anton <kokhanchik.tversu@gmail.com>
2017-05-12 17:58:36 +0300

    insertions

--- Luke/Luke/Form1.Designer.cs
+++ Luke/Luke/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Luke
+﻿namespace LukeSimple
 {
 	partial class Form1
 	{
@@ -28,12 +28,108 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBoxN = new System.Windows.Forms.TextBox();
+			this.buttonFromFile = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBoxNMinus1 = new System.Windows.Forms.TextBox();
+			this.buttonCalculate = new System.Windows.Forms.Button();
+			this.labelAnswer = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(60, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Введите N";
+			// 
+			// textBoxN
+			// 
+			this.textBoxN.Location = new System.Drawing.Point(12, 25);
+			this.textBoxN.Name = "textBoxN";
+			this.textBoxN.Size = new System.Drawing.Size(350, 20);
+			this.textBoxN.TabIndex = 1;
+			// 
+			// buttonFromFile
+			// 
+			this.buttonFromFile.Location = new System.Drawing.Point(287, 111);
+			this.buttonFromFile.Name = "buttonFromFile";
+			this.buttonFromFile.Size = new System.Drawing.Size(75, 23);
+			this.buttonFromFile.TabIndex = 2;
+			this.buttonFromFile.Text = "Из файла";
+			this.buttonFromFile.UseVisualStyleBackColor = true;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 48);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(269, 13);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Введите простые сомножители, составляющие N-1";
+			// 
+			// textBoxNMinus1
+			// 
+			this.textBoxNMinus1.Location = new System.Drawing.Point(15, 64);
+			this.textBoxNMinus1.Multiline = true;
+			this.textBoxNMinus1.Name = "textBoxNMinus1";
+			this.textBoxNMinus1.Size = new System.Drawing.Size(350, 41);
+			this.textBoxNMinus1.TabIndex = 1;
+			// 
+			// buttonCalculate
+			// 
+			this.buttonCalculate.Location = new System.Drawing.Point(137, 130);
+			this.buttonCalculate.Name = "buttonCalculate";
+			this.buttonCalculate.Size = new System.Drawing.Size(105, 40);
+			this.buttonCalculate.TabIndex = 2;
+			this.buttonCalculate.Text = "Проверить";
+			this.buttonCalculate.UseVisualStyleBackColor = true;
+			// 
+			// labelAnswer
+			// 
+			this.labelAnswer.AutoSize = true;
+			this.labelAnswer.Location = new System.Drawing.Point(174, 186);
+			this.labelAnswer.Name = "labelAnswer";
+			this.labelAnswer.Size = new System.Drawing.Size(0, 13);
+			this.labelAnswer.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(388, 272);
+			this.Controls.Add(this.buttonCalculate);
+			this.Controls.Add(this.buttonFromFile);
+			this.Controls.Add(this.textBoxNMinus1);
+			this.Controls.Add(this.textBoxN);
+			this.Controls.Add(this.labelAnswer);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBoxN;
+		private System.Windows.Forms.Button buttonFromFile;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBoxNMinus1;
+		private System.Windows.Forms.Button buttonCalculate;
+		private System.Windows.Forms.Label labelAnswer;
 	}
 }
 
--- Luke/Luke/Form1.cs
+++ Luke/Luke/Form1.cs
@@ -8,13 +8,47 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Luke
+namespace LukeSimple
 {
 	public partial class Form1 : Form
 	{
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void buttonFromFile_Click(object sender, EventArgs e)
+		{
+			openFileDialog1.ShowDialog();
+		}
+
+		private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+		{
+			textBoxN.Text = new System.IO.StreamReader(openFileDialog1.FileName).ReadToEnd();
+		}
+
+		private void buttonCalculate_Click(object sender, EventArgs e)
+		{
+			Calculate();
+		}
+
+		private void Calculate()
+		{
+			// чтение чисел
+			BigInt N = new BigInt(textBoxN.Text);
+			List<BigInt> q = new List<BigInt>();
+
+			string temp = "";
+			for(int i=0; i<textBoxNMinus1.Text.Length; i++)
+			{
+				if (Char.IsDigit(textBoxNMinus1.Text[i]))
+					temp += textBoxNMinus1.Text[i];
+				else if (textBoxNMinus1.Text[i] == ' ')
+					q.Add(new BigInt(temp));
+			}
+
+
+
 		}
 	}
 }
